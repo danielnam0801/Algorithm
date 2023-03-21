0ea5bda6a184343c5e4568baf801f33124bb0884
Nam junseong <danielnam0801@gmail.com>
2023-03-21 20:12:02 +0900

    2664, 2652

---
+++ Koistudy/2023-03-21/2023-03-21/배열의 인접한 원소차의 합 2652.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, m;
+	
+	cin >> n >> m;
+	if (n == 1){
+		cout << 0;
+	}
+	else if (n == 2) {
+		cout << m;
+	}
+	else
+		cout << m * 2;
+}
