5c9b3895d10fd230cc1d419a44bf0187d1774261
Nam junseong <danielnam0801@gmail.com>
2023-04-28 10:46:38 +0900

    st

---
+++ 100Joon/Stack/Stack/10773.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	stack<int> str;
+	
+	for (int i = 0; i < n; i++) {
+		int num;
+		cin >> num;
+		if (num == 0) {
+			str.pop();
+		}
+		else str.push(num);
+	}
+
+	int sum = 0;
+	while (!str.empty()) {
+		sum += str.top();
+		str.pop();
+	}
+
+	cout << sum;
+}
