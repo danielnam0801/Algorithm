d62240f8936745c70f23178e37ae861864938d11
Nam junseong <danielnam0801@gmail.com>
2023-07-08 23:35:08 +0900

    9935

--- 100Joon/Gold/Gold/9935.cpp
+++ 100Joon/Gold/Gold/9935.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include <unordered_map>
+#include <map>
 
 using namespace std;
 
@@ -10,48 +12,13 @@ int main() {
 	cin.tie(NULL);
 	ios_base::sync_with_stdio(false);
 
-	string s1, s2;
+	string str, bomb, answer;
 
-	cin >> s1;
-	cin >> s2;
+	cin >> str;
+	cin >> bomb;
 
-	vector<int> indexList;
-
-	for (int i = 0; i < s2.size(); i++) {
-		int pos = 0;
-		while (true){
-			
-			int index = s1.find(s2[i], pos);
-			
-			if (index == string::npos) {
-				break;
-			}
-			else {
-				indexList.push_back(index);
-				pos = index + 1;
-			}
-		}
-	}
-	
-	if (indexList.size() == s1.size()) {
-		cout << "FRULA";
-		return 0;
-	}
-	else {
+	int cnt = 0;
+	for (int i = 0; i < str.size(); i++) {
 		
-		sort(indexList.begin(), indexList.end());
-		
-		int n = 0;
-		for (int i = 0; i < s1.size(); i++) {
-			if (n < indexList.size()){
-				if (i == indexList[n]) {
-					n++;
-					continue;
-				}
-			}
-			cout << s1[i];
-		}
 	}
-
-
 }
