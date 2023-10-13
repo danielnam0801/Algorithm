377f007bed4222a176275afff4f437fc06a1eafc
Nam junseong <danielnam0801@gmail.com>
2023-10-13 23:39:44 +0900

    1920수정

--- Baekjoon/Baekjoon/S4-1920.cpp
+++ Baekjoon/Baekjoon/S4-1920.cpp
@@ -1,11 +1,12 @@
-#include <iostream>
 #include <algorithm>
 #include <vector>
+#include <iostream>
 using namespace std;
+
 int n, vn;
 int m, vm;
 vector<int> v;
-vector<int> v2;
+
 int bisearch(int x) {
 	int low = 0;
 	int high = n - 1;
@@ -14,7 +15,7 @@ int bisearch(int x) {
 		int mid = (low + high) / 2;
 		if (v[mid] == x) return 1;
 		else if (v[mid] > x) high = mid - 1;
-		else if (v[mid] < x) low = mid - 1;
+		else if (v[mid] < x) low = mid + 1;
 	}
 	return 0;
 }
@@ -33,33 +34,5 @@ int main() {
 		cin >> vm;
 		cout << bisearch(vm) << "\n";
 	}
-
-	//sort(arr1, arr1 + n);
-	//for (int i = 0; i < m; i++) {
-	//	re:
-	//	int low = arr1[0];
-	//	int high = arr1[n-1];
-	//	int mid = arr1[low + high / 2];
-
-	//	for (int j = 0; j < n; j++) {
-	//		if (arr2[i] == arr1[mid]) {
-	//			cout << 1 << "\n";
-	//			++i;
-	//			goto re;
-	//			break;
-	//		}
-	//		else if (arr2[i] < arr1[mid]) {
-	//			high = mid - 1;
-	//		}
-	//		else if (arr2[i] > arr1[mid]) {
-	//			low = mid + 1;
-	//		}
-	//	}
-	//	cout << 0 << "\n";
-	//}
-
-	//delete[] arr1;
-	//delete[] arr2;
-
 }
 
