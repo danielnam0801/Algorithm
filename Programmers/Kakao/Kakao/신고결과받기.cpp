55200adc99ab1dbfa42593b55d7eda3aff5bac06
Nam junseong <danielnam0801@gmail.com>
2023-11-06 11:09:59 +0900

    신고결과받기

--- Programmers/Kakao/Kakao/신고결과받기.cpp
+++ Programmers/Kakao/Kakao/신고결과받기.cpp
@@ -2,15 +2,15 @@
 
 #include<vector>
 #include<sstream>
-#include<set>
+#include<unordered_set>
 #include<unordered_map>
 
 using namespace std;
 
 unordered_map<string, int> reported_list; // 신고 받은 사람 // 신고수
-unordered_map<string, set<string>> reporter_list; // key : 신고한 사람 // value : 신고 대상
+unordered_map<string, unordered_set<string>> reporter_list; // key : 신고한 사람 // value : 신고 대상
 
-vector<int> solution(vector<string> id_list, vector<string> report, int k) 
+vector<int> solution(vector<string> id_list, vector<string> report, int k)
 {
     vector<int> answer;
 
@@ -30,7 +30,7 @@ vector<int> solution(vector<string> id_list, vector<string> report, int k)
         reported_list[reported]++; // 신고수 증가
     }
 
-    for(size_t i = 0; i < id_list.size(); i++)
+    for (size_t i = 0; i < id_list.size(); i++)
     {
         int result = 0;
         for (const auto& target : reporter_list[id_list[i]])
